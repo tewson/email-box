36ba2056d6cafdcf4464b9235def2f0ab435b6d6
Tewson Seeoun <tewson.seeoun@gmail.com>
2018-05-29 22:53:48 +0100

    Render children

--- src/EmailBox.test.js
+++ src/EmailBox.test.js
@@ -12,4 +12,27 @@ describe("EmailBox", () => {
   it("should render correctly", () => {
     expect(toJson(shallow(<EmailBox />))).toMatchSnapshot();
   });
+
+  it("should render children correctly", () => {
+    expect(
+      toJson(
+        shallow(
+          <EmailBox>
+            <div>
+              <h1>Test</h1>
+              <p>Children</p>
+            </div>
+          </EmailBox>
+        )
+      )
+    ).toMatchSnapshot();
+
+    const randomChild = <p>{Math.random()}</p>;
+
+    expect(
+      shallow(<EmailBox>{randomChild}</EmailBox>).containsMatchingElement(
+        randomChild
+      )
+    ).toBeTruthy();
+  });
 });
